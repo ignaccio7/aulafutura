450c9586f250fbe5747deac5232bccc49d2febba
ignaccio7 <nacho260298@gmail.com>
2026-02-20 21:19:15 -0400

    feat: add product model

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    protected $fillable = [
+        'category_id',
+        'title',
+        'description',
+        'price',
+        'type',
+        'thumbnail',
+        'is_active',
+    ];
+
+    // Producto pertenece a categoría
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relación 1 a 1 con book_files
+    public function bookFile()
+    {
+        return $this->hasOne(BookFile::class);
+    }
+}
